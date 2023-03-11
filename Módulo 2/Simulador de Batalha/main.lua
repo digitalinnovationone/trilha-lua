3b926c3441a0671a8ae63f94e41bbbfe68ee40fa
Rafa Skoberg <rafa.skoberg@gmail.com>
2023-03-11 10:36:55 -0300

    Primeiro arquivo do Simulador de Batalha

---
+++ Módulo 2/Simulador de Batalha/main.lua
@@ -0,0 +1,52 @@
+--[[
+    | Prismarine Colossus
+    | 
+    | Enormes estátuas de prismarine que medem mais de 20 metros de altura. Sua aparência ameaçadora pode assustar até os mais bravos guerreiros.
+    | Eles têm olhos turquesa brilhantes, mas um elogio não vai te ajudar muito durante uma batalha.
+    | 
+    | Atributos
+    | Vida      ▰▰▰▰▰▰▰▰▰▰
+    | Ataque    ▰▰▰▰▱▱▱▱▱▱
+    | Defesa    ▰▰▰▰▰▰▰▰▰▰
+    | Agilidade ▰▱▱▱▱▱▱▱▱▱
+
+    O que você vai fazer?
+    1. Atacar com a espada.
+    2. Usar poção de regeneração.
+    3. Atirar uma pedra.
+    4. Se esconder.
+    >
+
+]]
+
+-- Habilitar UTF-8 no terminal
+os.execute("chcp 65001")
+
+-- Header
+print([[
+=====================================================================
+               _
+    _         | |
+   | | _______| |---------------------------------------------\
+   |:-)_______|==[]============================================>
+   |_|        | |---------------------------------------------/
+              |_|
+
+               -----------------------------------------
+
+                     ⚔ SIMULADOR DE BATALHA ⚔
+
+=====================================================================
+
+            Você empunha sua espada e se prepara para lutar.
+                         É hora da batalha!
+
+]])
+
+-- Obter definição do jogador
+
+-- Obter definição do monstro
+
+-- Apresentar o monstro
+
+-- Começar o loop de batalha
